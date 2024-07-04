fae6d1e8cd3abdaca65371e929293bda390da599
Surve <surve8729@gmail.com>
2024-07-05 00:36:30 +0530

    Add NewSketch & Reset Button

--- script.js
+++ script.js
@@ -1,30 +1,50 @@
 const container = document.querySelector(".container");
-const gridPref = 13;
+const newButton = document.querySelector(".newButton");
+const resetButton = document.querySelector(".resetButton");
+
+let gridPref = 10;
 const boxSize = 30;
 let containerWidth = (boxSize+2) * gridPref;
 container.style.width = `${containerWidth}px`
-console.log(containerWidth)
+// console.log(containerWidth)
 // const userGridPref = prompt('Whats your preferred grid?');
 
-
 function createSquares() {
-    const square = document.createElement("div");
-    square.style.width = `${boxSize}px`;
-    square.style.height = `${boxSize}px`;
-    square.style.backgroundColor ="#a8a4b5";
-    square.style.border = "1px dotted gray";
+    container.innerHTML = "";
+    for (let i = 0; i < (gridPref*gridPref); i++){
+        const square = document.createElement("div");
+        square.style.width = `${boxSize}px`;
+        square.style.height = `${boxSize}px`;
+        square.style.backgroundColor ="#a8a4b5";
+        square.style.border = "1px dotted gray";
 
-    container.appendChild(square);
+        container.appendChild(square);
 
-    square.addEventListener("mouseover", (event) => {
-        console.log("hover!")
-        event.target.style.backgroundColor = "#fef900";
-    })
+        square.addEventListener("mouseover", (event) => {
+            // console.log("hover!")
+            event.target.style.backgroundColor = "#fef900";
+        })
+        }
+
+    
+
 }
 
-
-for (let i = 0; i < (gridPref*gridPref); i++){
+function resetGrid(){
     createSquares();
 }
 
+function newGrid(){
+    gridPref = prompt('Whats your preferred grid?');
+    containerWidth = (boxSize+2) * gridPref;
+    container.style.width = `${containerWidth}px`;
+    
+    console.log(gridPref, containerWidth);
+    createSquares();
+}
+
+createSquares();
+
+newButton.addEventListener("click", newGrid);
+resetButton.addEventListener("click", resetGrid);
 
